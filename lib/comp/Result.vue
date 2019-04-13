aff3124724cfb0e9f3c96f3de12e451980d593e7
Shigma <1700011071@pku.edu.cn>
2019-04-14 01:25:55 +0800

    adjustments

--- lib/comp/Result.vue
+++ lib/comp/Result.vue
@@ -3,72 +3,57 @@
     <h3 class="tac">恭喜您完成 {{ level }} 难度测试！</h3>
     <h2 class="tac">您的正确率：{{ correctNum }}/{{ questions.length }}</h2>
     <div class="button-container tac">
-      <Button
-        class="res-page-back-btn"
-        title="返回主界面"
-        type="warning"
-        @click="backToSettings"
-      >
+      <Button type="warning" @click="$emit('next', 'Settings')">
         返回主界面
       </Button>
     </div>
-    <div class="answer-results">
-      <div class="hint tac">点击题目查看选项及解析。</div>
+    <div class="section">
       <div class="table-header">
-        <div class="header-item qid">题号</div>
-        <div class="header-item header-question">题目</div>
-        <div class="header-item answer">您的答案</div>
-        <div class="header-item answer">正确答案</div>
+        <div class="th qid">题号</div>
+        <div class="th question">题目</div>
+        <div class="th answer">您的答案</div>
+        <div class="th answer">正确答案</div>
       </div>
       <CollapseView
-        class="container"
-        v-for="(question, qid) in shuffledQuestions"
+        v-for="(question, qid) in questions"
         :key="qid"
         :initial="wrongIds.includes(qid) ? 'open' : 'close'"
       >
         <div class="cv-header" slot="header">
-          <div class="header-item qid">{{ qid + 1 }}</div>
-          <div class="header-item header-question">{{ question[0] }}</div>
-          <div class="header-item answer answer-cont" :class="wrongIds.includes(qid) ? 'incorrect' : 'correct'">
+          <div class="td qid">{{ qid + 1 }}</div>
+          <div class="td question">{{ question[0] }}</div>
+          <div class="td answer" :class="wrongIds.includes(qid) ? 'incorrect' : 'correct'">
             {{ String.fromCharCode(answers[qid] + 65) }}
           </div>
-          <div class="header-item answer answer-cont correct">
-            {{ String.fromCharCode(question[1].indexOf(questions[qid][1][0]) + 65) }}
+          <div class="td answer correct">
+            {{ String.fromCharCode(question._answerIndex + 65) }}
           </div>
         </div>
         <div class="cv-body">
-          <div class="body-question">
-            <div class="cv-body-item">题目</div>
-            <div class="cv-body-content">{{ question[0] }}</div>
+          <div class="tr question">
+            <span class="item">题目</span>
+            <span class="detail">{{ question[0] }}</span>
           </div>
           <div
-            class="choice-container"
+            class="tr"
             v-for="(choice, cid) in question[1]"
             :key="cid"
           >
-            <div class="cv-body-item">选项{{ String.fromCharCode(cid + 65) }}</div>
-            <div class="cv-body-content">{{ choice }}</div>
+            <span class="item">选项{{ String.fromCharCode(cid + 65) }}</span>
+            <span class="detail">{{ choice }}</span>
           </div>
-          <div class="explanation-container">
-            <div class="cv-body-item">解析</div>
-            <div class="cv-body-content explanation">{{ question[2] }}</div>
+          <div class="tr explanation-container">
+            <span class="item">解析</span>
+            <span class="detail explanation">{{ question[2] }}</span>
           </div>
         </div>
       </CollapseView>
     </div>
-    <div class="button-container tac">
-      <Button
-        class="res-page-back-btn"
-        title="返回主界面"
-        type="warning"
-        @click="backToSettings"
-      >
+    <div class="button-container">
+      <Button type="warning" @click="$emit('next', 'Settings')">
         返回主界面
       </Button>
-      <Button
-        class="res-page-back-btn"
-        @click="$router.push(UZKK_QUIZ_BASE + 'about.html')"
-      >
+      <Button @click="$router.push(UZKK_QUIZ_BASE + 'about.html')">
         关于知识测试
       </Button>
     </div>
@@ -77,195 +62,132 @@
 
 <script>
 
-import Button from '@theme-uzkk/components/Button'
-import CollapseView from '@theme-uzkk/components/CollapseView'
+import { Button, CollapseView } from '@uzkk/components'
 
 export default {
-  name: 'Result',
-
-  components: {
-    Button,
-    CollapseView,
-  },
+  components: { Button, CollapseView },
 
   props: [
     'level',
     'questions',
-    'shuffledQuestions',
     'correctNum',
     'wrongIds',
     'answers',
   ],
-
-  methods: {
-    backToSettings () {
-      this.$emit('next', 'Settings')
-    },
-  },
 }
 
 </script>
 
 <style lang="stylus" scoped>
 
-.tac
-  text-align center
-
-.res-page-back-btn
-  width 100%
-  display block
-  margin 0.8em 0
-
-.answer-results
-  margin 2em auto
-  padding 0.5em 1em 1.5em
-  border-radius 1em
-  background-color #fff
-
-.hint
-  margin 1em auto
-  color #aaa
-  font-style italic
-
 .table-header
-  @media (min-width 639px)
-    padding 0.5em 2em
-  @media (min-width 399px) and (max-width 639px)
-    padding 0.5em 1em
   margin 0 auto
-  max-width 1080px
   color #888
   font-size 1.1em
   font-weight bold
+  @media (max-width $MQMobileNarrow)
+    padding 0.5em 0
+  @media (min-width $MQMobileNarrow) and (max-width $MQMobile)
+    padding 0.5em 1em
+  @media (min-width $MQMobile)
+    padding 0.5em 2em
+
+.collapse-view
+  margin 0 auto 0.5em
+  &:last-child
+    margin-bottom 0
+  @media (max-width $MQMobileNarrow)
+    padding 0 0
+  @media (min-width $MQMobileNarrow) and (max-width $MQMobile)
+    padding 0 1em
+  @media (min-width $MQMobile)
+    padding 0 2em
+
+.th, .td
+  display inline-block
+  text-align center
+  vertical-align middle
+
+  &.qid
+    @media (max-width $MQMobile)
+      width 26%
+    @media (min-width $MQMobile)
+      width 15%
+
+  &.question
+    width 50%
+    @media (max-width $MQMobile)
+      display none
+
+  &.answer
+    @media (max-width $MQMobile)
+      width 34%
+    @media (min-width $MQMobile)
+      width 15%
+
+.td
+  line-height 1.6
+
+  @media (max-width $MQMobile)
+    &.qid::before
+      content '第'
+      padding-right 2px
+    &.qid::after
+      content '题'
+      padding-left 2px
+
+  &.answer
+    @media (min-width $MQMobile)
+      font-size 1.2em
 
 .cv-header
-  @media (max-width 399px)
-    padding 0.1em 0
-  @media (min-width 399px) and (max-width 639px)
-    padding 0.2em 0
-  @media (min-width 639px) and (max-width 899px)
-    padding 0.35em 0
-  @media (min-width 899px)
-    padding 0.5em 0
   border-top 1px solid #ddd
   border-bottom 1px solid #ddd
   background-color #f7f7f7
+  @media (max-width $MQMobileNarrow)
+    padding 0.1em 0
+  @media (min-width $MQMobileNarrow) and (max-width $MQMobile)
+    padding 0.2em 0
+  @media (min-width $MQMobile)
+    padding 0.35em 0
 
-.header-item
-  display inline-block
-  vertical-align middle
-  text-align center
+.tr
+  width 100%
+  display inline-flex
+  margin-top 0.5em
+  line-height 1.5
+  padding-right 1em
+  @media (max-width $MQMobile)
+    padding-right 0.5em
 
-.qid
-  @media (max-width 299px)
-    width 20%
-  @media (min-width 299px) and (max-width 639px)
-    width 26%
-  @media (min-width 639px)
-    width 15%
+  &.question
+    font-weight bold
+    @media (min-width $MQMobile)
+      display none
 
-.header-question
-  @media (max-width 639px)
-    display none
-  @media (min-width 639px)
-    width 50%
-
-.answer
-  @media (max-width 299px)
-    width 35.5%
-  @media (min-width 299px) and (max-width 639px)
-    width 34%
-  @media (min-width 639px)
-    width 15%
-
-.answer-cont
-  @media (min-width 639px)
-    font-size 1.2em
-  @media (min-width 399px) and (max-width 639px)
-    font-size 1.08em
-
-.cv-body
-  @media (max-width 399px)
-    padding 0.5em 0.75em
-  @media (min-width 399px) and (max-width 639px)
-    padding 0.5em 1.5em
-  @media (min-width 639px) and (max-width 899px)
-    padding 0.5em 2.5em
-  @media (min-width 899px)
-    padding 0.5em 5em
-
-.body-question
-  @media (min-width 639px)
-    display none
-  font-weight bold
-
-.cv-body-item
-  font-weight bold
-  color #bbb
-  @media (min-width 299px)
+  .item
     display inline-block
-    width 8%
-    min-width 55px
-    vertical-align top
-  @media (max-width 299px)
+    font-weight bold
+    color #bbb
     text-align center
-    margin-bottom 0.1em
+    vertical-align middle
+    @media (max-width $MQMobile)
+      width 6em
+    @media (min-width $MQMobile)
+      width 15%
 
-.cv-body-content
-  display inline-block
-  vertical-align top
-  margin-bottom 0.5em
-  @media (min-width 853px)
-    width 90%
-  @media (min-width 484px) and (max-width 853px)
-    width 80%
-  @media (min-width 361px) and (max-width 484px)
-    width 75%
-  @media (min-width 299px) and (max-width 361px)
-    width 70%
-  @media (max-width 299px)
-    display block
-    text-align center
-    margin-bottom 0.75em
-
-.explanation
-  font-style italic
+  .detail
+    display inline-block
+    vertical-align middle
+    @media (max-width $MQMobile)
+      width inherit
+    @media (min-width $MQMobile)
+      width 85%
 
 .correct
   color #0c0
 
 .incorrect
   color #e00
-
-.container
-  margin 0 auto
-  @media (min-width 639px)
-    padding 0.3em 2em
-  @media (min-width 399px) and (max-width 639px)
-    padding 0.3em 1em
-  @media (max-width 399px)
-    padding 0.3em 0
-  max-width 1080px
-
-  .opened-header
-    color #55c
-
-  .closed-header
-    color #000
-
-    &:hover
-      color #55c
-
-  > :first-child
-    margin-top 0
-
-  > :last-child
-    margin-bottom 0
-
-  h3
-    margin 0
-    padding 0.2em
-    background-color #eee
-    border-radius 0.5em
 
 </style>
