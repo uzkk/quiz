908fe986f96ac7b0e50f3506e2cc62b75a09f5ba
Shigma <1700011071@pku.edu.cn>
2019-04-15 15:41:00 +0800

    allow for incompletion

--- lib/comp/index.vue
+++ lib/comp/index.vue
@@ -31,9 +31,7 @@ export default {
 
   methods: {
     submit () {
-      if (this.questions.filter(q => q.choice < 0).length > 0) {
-        confirm('您有未完成的题目，请在“选择题号”界面查看并完成它们。')
-      } else {
+      if (this.questions.filter(q => q.choice < 0).length === 0 || confirm('您有未完成的题目，确认现在提交吗？')) {
         this.phase = 'Result'
       }
     },
